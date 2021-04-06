409eca7f9a5310361e6cdd5da470c515053fe8f8
Tyler Madonna <tjm159@pitt.edu>
2021-04-06 15:32:30 -0400

    Updated build python message defs tool to be platform independent

--- tools/build_python_message_defs.py
+++ tools/build_python_message_defs.py
@@ -1,29 +1,31 @@
-
 import os
-import platform
+import sys
 import subprocess as sp
 from argparse import ArgumentParser
 
-if __name__ == "__main__":
-    parser = ArgumentParser(description = "Converts C header files to python files")
+if __name__ == '__main__':
+
+    description = 'Converts C header files to python files'
+    parser = ArgumentParser(description = description)
     parser.add_argument(type=str, dest='input_file')
     args = parser.parse_args()
-    print("Processing %s ..." % (args.input_file))
+    print('Processing %s ...' % (args.input_file))
 
     input_file = os.path.abspath(args.input_file)
     base_dir, filename = os.path.split(input_file)
     filename_root, filename_ext = os.path.splitext(filename)
-    
-    os_name = platform.system()
-    if os_name == "Linux":
-        xml_file = os.path.join(base_dir, filename_root) + '.xml'
-        sp.call(['h2xml', '-c', '-o', xml_file, input_file])
-        out_file = os.path.join(base_dir, filename_root) + '.py'
-        sp.call(['xml2py', '-o', out_file, xml_file])
 
-    elif os_name == "Windows":
-        ctypesgen_path = os.environ['CTYPESGEN'] + '\\run.py'
+    ctypesgen = os.getenv('CTYPESGEN')
+    if ctypesgen:
+        ctypesgen_path = os.path.join(ctypesgen, 'run.py')
         output_file = os.path.join(base_dir, filename_root) + '.py'
-        python_exe = os.environ['PYTHON_BASE'] + '\\Python.exe'
-        sp.call([python_exe, ctypesgen_path, '--includedir="..\\include"', '-a', '-o', output_file, input_file])
-        
+
+        print('%s')
+
+        python_exe = sys.executable
+        include_path = os.path.join('..', 'include')
+        proc = sp.call([python_exe, ctypesgen_path, 
+                       '--includedir="%s"' % (include_path), 
+                       '-a', '-o', output_file, input_file])
+    else:
+        print('Error: CTYPESGEN environment variable must be set')
